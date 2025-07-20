171c04876d1d42b62a7b23a51d99a715fd40aa85
Ana Amarante <kacaamarante@hotmail.com>
2025-07-20 12:14:28 -0300

    Adicionar texto e imagem na home, cor de background

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NavBarComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
