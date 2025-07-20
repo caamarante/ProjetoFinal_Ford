893cfd0cf2426675fb79926bf74d7fbab0f1dd06
Ana Amarante <kacaamarante@hotmail.com>
2025-07-20 13:46:32 -0300

    Criar pagina game, textos de instrucoes, chamar cards, adicionar scroll na div para a pagina se manter estatica

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
+import { FooterComponent } from "../../components/footer/footer.component";
+import { CardTrofeuComponent } from "../../components/card-trofeu/card-trofeu.component";
 
 @Component({
   selector: 'app-game',
-  imports: [],
+  imports: [NavBarComponent, FooterComponent, CardTrofeuComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
