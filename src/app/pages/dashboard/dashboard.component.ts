e3861f9fe62fc158af78bc54842b26b0d565c3ca
Ana Amarante <kacaamarante@hotmail.com>
2025-07-22 23:02:36 -0300

    Criar componente da dashboard que aparece depois do login realizado

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,56 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
+import { FooterComponent } from "../../components/footer/footer.component";
+import { Router } from '@angular/router';
+import { CardTrofeuComponent } from "../../components/card-trofeu/card-trofeu.component";
+import { CardCarrosComponent } from "../../components/card-carros/card-carros.component";
+
+interface Carro {
+  id: string;
+  nome: string;
+  imagem: string;
+  descricao: string;
+}
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [NavBarComponent, FooterComponent, CardTrofeuComponent, CardCarrosComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+
+export class DashboardComponent implements OnInit {
+  nome = localStorage.getItem('Nome');
+  pontuacao = Number(localStorage.getItem('potuacao'));
+  carroEscolhidoObj: Carro | null = null;
+  @Input() imagemTrofeu: string = ""
+  @Input() nomeTrofeu: string = ""
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    if (localStorage.getItem('isAuthenticated') !== 'true') {
+      this.router.navigate(['/login']);
+    }
+    const carroEscolhidoString = localStorage.getItem('carroEscolhido');
+    if (carroEscolhidoString) {
+      this.carroEscolhidoObj = JSON.parse(carroEscolhidoString);
+    }
+    if(this.pontuacao <= 100 ){
+      this.imagemTrofeu = 'assets/trofeu-bronze.png'
+      this.nomeTrofeu = "Trófeu de Bronze"
+    } else if (this.pontuacao > 100 && this.pontuacao <= 300){
+      this.imagemTrofeu = 'assets/trofeu-prata.png'
+      this.nomeTrofeu = "Trófeu de Prata"
+    } else {
+      this.imagemTrofeu = 'assets/trofeu-ouro.png'
+      this.nomeTrofeu = "Trófeu de Ouro"
+    }
+
+  }
+
+  onLogout(): void {
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
+}
