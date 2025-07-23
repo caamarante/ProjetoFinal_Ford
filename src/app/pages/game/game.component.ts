c3e0c06e6f9b45915c223ab95bcee069f1260294
Ana Amarante <kacaamarante@hotmail.com>
2025-07-22 23:03:33 -0300

    Estilizar e adicionar mais uma etapa antes do quiz

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
 import { FooterComponent } from "../../components/footer/footer.component";
 import { CardTrofeuComponent } from "../../components/card-trofeu/card-trofeu.component";
+import { LoginFormsComponent } from "../../components/login-forms/login-forms.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-game',
-  imports: [NavBarComponent, FooterComponent, CardTrofeuComponent],
+  imports: [NavBarComponent, FooterComponent, CardTrofeuComponent, LoginFormsComponent, CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
-export class GameComponent {
+export class GameComponent implements OnInit{
+  loginFeito = false;
+
+  ngOnInit(): void {
+    localStorage.setItem('quizFinalizado', 'false');
+    const loginFeito = Boolean(localStorage.getItem("isAuthenticated"));
+    this.loginFeito = loginFeito;
+  }
 
 }
