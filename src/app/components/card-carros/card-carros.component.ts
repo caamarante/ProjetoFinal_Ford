eb37493a008b1b0b75e27fd3f44774b7378d646f
Ana Amarante <kacaamarante@hotmail.com>
2025-07-22 22:58:41 -0300

    Criar componente do card de carros que sera utilizado no selectGame e no dashboard

--- src/app/components/card-carros/card-carros.component.ts
+++ src/app/components/card-carros/card-carros.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-card-carros',
@@ -7,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './card-carros.component.css'
 })
 export class CardCarrosComponent {
+  @Input() imagem: string = ""
+  @Input() legenda: string = ""
+  @Input() carro: string = ""
+  @Input() descricao: string = ""
+  @Input() id: string = ""
 
+  @Output() carroSelecionado = new EventEmitter<any>();
+
+  selecionarCarro() {
+    const carroInfo = {
+      id: this.id,
+      nome: this.carro,
+      imagem: this.imagem,
+      descricao: this.descricao
+    };
+    this.carroSelecionado.emit(carroInfo);
+  }
 }
