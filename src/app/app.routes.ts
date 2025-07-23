a760968baec015956981e42834eda7f498434210
Ana Amarante <kacaamarante@hotmail.com>
2025-07-22 22:57:48 -0300

    Adicionar caminhos de rota para novos components e verificacoes de autenticacao

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { GameComponent } from './pages/game/game.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SelectGameComponent } from './pages/select-game/select-game.component';
+import { QuizComponent } from './pages/quiz/quiz.component';
+import { SelectGameAccessGuard } from './services/navigation-guard.service';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'game', component: GameComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'selectGame', component: SelectGameComponent, canActivate: [SelectGameAccessGuard]  },
+  { path: 'quiz', component: QuizComponent },
+  { path: 'dashboard', component: DashboardComponent },
+
 ];
